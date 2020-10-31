767f66eba5f61b9acb36e9b85596b4679b72b7ff
Kengo Wada <msf.1192@gmail.com>
2020-10-31 21:47:59 +0900

    JavaDocの追加

--- src/main/java/net/den3/den3Account/AdminAccount.java
+++ src/main/java/net/den3/den3Account/AdminAccount.java
@@ -3,10 +3,19 @@ package net.den3.den3Account;
 public class AdminAccount extends AccountEntity implements IAccount {
     private boolean admin = false;
 
+    /**
+     * 管理者権限の有無を返すメソッド
+     * @return true→管理者 / false→非管理者
+     */
     public boolean isAdmin() {
         return admin;
     }
 
+    /**
+     * アカウントに管理者権限を設定する
+     * @param isAdmin true→管理者 / false→非管理者
+     * @return AdminAccountのインスタンス
+     */
     public AdminAccount setAdminAccount(boolean isAdmin){
         this.admin = isAdmin;
         return this;
