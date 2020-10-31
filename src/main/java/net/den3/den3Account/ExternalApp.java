65d6f39b920639aac79ac8dc5860c522e597bc03
Kengo Wada <msf.1192@gmail.com>
2020-11-01 01:51:03 +0900

    外部連携アプリの利用する権限を表現 外部連携アプリのインターフェイスを実装

--- src/main/java/net/den3/den3Account/ExternalApp.java
+++ src/main/java/net/den3/den3Account/ExternalApp.java
@@ -1,13 +1,89 @@
 package net.den3.den3Account;
 
-public class ExternalApp {
+import java.util.ArrayList;
+import java.util.List;
+
+enum Permission{
+    READ_ID,
+    READ_MAIL
+}
+
+public class ExternalApp implements IApp {
     private String AppID = "";
     private String AdminID = "";
     private String AppName = "";
     private String RedirectURL = "";
     private String AppIconURL = "";
     private String AppDescription = "";
-    private boolean PermissionMailAddress = false;
+    private List<Permission> UsedPermission = new ArrayList<>();
 
+    @Override
+    public String getAppID() {
+        return AppID;
+    }
 
+    public ExternalApp setAppID(String appID) {
+        AppID = appID;
+        return this;
+    }
+
+    @Override
+    public String getAdminID() {
+        return AdminID;
+    }
+
+    public ExternalApp setAdminID(String adminID) {
+        AdminID = adminID;
+        return this;
+    }
+
+    @Override
+    public String getAppName() {
+        return AppName;
+    }
+
+    public ExternalApp setAppName(String appName) {
+        AppName = appName;
+        return this;
+    }
+
+    @Override
+    public String getRedirectURL() {
+        return RedirectURL;
+    }
+
+    public ExternalApp setRedirectURL(String redirectURL) {
+        RedirectURL = redirectURL;
+        return this;
+    }
+
+    @Override
+    public String getAppIconURL() {
+        return AppIconURL;
+    }
+
+    public ExternalApp setAppIconURL(String appIconURL) {
+        AppIconURL = appIconURL;
+        return this;
+    }
+
+    @Override
+    public String getAppDescription() {
+        return AppDescription;
+    }
+
+    public ExternalApp setAppDescription(String appDescription) {
+        AppDescription = appDescription;
+        return this;
+    }
+
+    @Override
+    public List<Permission> getUsedPermission() {
+        return UsedPermission;
+    }
+
+    public ExternalApp setUsedPermission(List<Permission> usedPermission) {
+        UsedPermission = usedPermission;
+        return this;
+    }
 }
