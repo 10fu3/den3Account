dcbd146e8142ae44addb09780a79880c92e756f7
Kengo Wada <msf.1192@gmail.com>
2020-11-01 01:31:44 +0900

    外部連携アプリを管理するクラスを実装

---
+++ src/main/java/net/den3/den3Account/ExternalApp.java
@@ -0,0 +1,13 @@
+package net.den3.den3Account;
+
+public class ExternalApp {
+    private String AppID = "";
+    private String AdminID = "";
+    private String AppName = "";
+    private String RedirectURL = "";
+    private String AppIconURL = "";
+    private String AppDescription = "";
+    private boolean PermissionMailAddress = false;
+
+
+}
