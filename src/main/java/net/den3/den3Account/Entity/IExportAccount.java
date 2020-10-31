51e59b80985f8d0a654d569614f6d1347c6f04d8
Kengo Wada <msf.1192@gmail.com>
2020-10-31 23:06:07 +0900

    外部サービスと連携するためのクラス

---
+++ src/main/java/net/den3/den3Account/Entity/IExportAccount.java
@@ -0,0 +1,36 @@
+package net.den3.den3Account.Entity;
+
+/**
+ * 外部サービスとやりとりするためのアカウントを表すクラス
+ */
+public class IExportAccount extends AccountEntity {
+
+    /**
+     * 外部サービスとやりとりするアカウントの情報をJSON出力する
+     * @return JSON ユニークID(id) ニックネーム(name) アイコン画像のURL(icon)
+     */
+    public String getJSON(){
+        return buildAccount().toString();
+    }
+
+    private StringBuilder buildAccount(){
+        return new StringBuilder()
+                .append("{")
+                .append(buildWord("id",super.getUUID()))
+                .append(" , ")
+                .append(buildWord("name",super.getNickName()))
+                .append(" , ")
+                .append(buildWord("icon",super.getIconURL()))
+                .append("}");
+    }
+    private StringBuilder buildWord(String key,String value){
+        return new StringBuilder()
+                .append("\"")
+                .append(key)
+                .append("\"")
+                .append(" : ")
+                .append("\"")
+                .append(value)
+                .append("\"");
+    }
+}
