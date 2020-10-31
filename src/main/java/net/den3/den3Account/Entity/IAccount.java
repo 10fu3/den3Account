eafac128172b1cf19130a760cbaa896d52bbed8a
Kengo Wada <msf.1192@gmail.com>
2020-10-31 23:11:22 +0900

    ユニークIDを実装

--- src/main/java/net/den3/den3Account/Entity/IAccount.java
+++ src/main/java/net/den3/den3Account/Entity/IAccount.java
@@ -3,11 +3,10 @@ package net.den3.den3Account.Entity;
 import java.util.Date;
 
 public interface IAccount {
+    String getUUID();
     String getMailAddress();
     String getPasswordHash();
     String getNickName();
     String getIconURL();
     String getLastLoginTime();
-
-
 }
