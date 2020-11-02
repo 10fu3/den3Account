eaf734ff30523b2ae256902f97847ec2a8d22972
Kengo Wada <msf.1192@gmail.com>
2020-11-03 03:52:28 +0900

    独自の結果表現クラスの使用をやめて,Optionalに移行

--- src/main/java/net/den3/den3Account/Logic/IStore.java
+++ src/main/java/net/den3/den3Account/Logic/IStore.java
@@ -1,15 +1,15 @@
 package net.den3.den3Account.Logic;
 
 import net.den3.den3Account.Entity.IAccount;
-import net.den3.den3Account.Entity.Result;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
 
 public interface IStore {
-    Result<IAccount> getAccountByMail(String mail);
-    Result<IAccount> getAccountByUUID(String id);
-    Result<List<IAccount>> getAccountBySQL(Function<Connection, PreparedStatement> query);
+    Optional<IAccount> getAccountByMail(String mail);
+    Optional<IAccount> getAccountByUUID(String id);
+    Optional<List<IAccount>> getAccountBySQL(Function<Connection, PreparedStatement> query);
 }
