acbad90a571ca1062bdc1bb0ee52a1adac386d0c
Kengo Wada <msf.1192@gmail.com>
2020-11-03 03:55:32 +0900

    静的メソッドとしてメールアドレス/パスワードのバリデーションを追加

--- src/main/java/net/den3/den3Account/Logic/RegisterAccount.java
+++ src/main/java/net/den3/den3Account/Logic/RegisterAccount.java
@@ -1,5 +1,29 @@
 package net.den3.den3Account.Logic;
 
+import net.den3.den3Account.StringChecker;
+
+
 public class RegisterAccount {
+    public RegisterAccount(){
+
+    }
+
+    public static CheckAccountResult checkAccount (String mail,String pass){
+        if(!StringChecker.isMailAddress(mail)){
+            //return "{ \"status\" : \"ERROR\" , \"message\" : \"Invalid e-address\" }";
+            return CheckAccountResult.ERROR_MAIL;
+        }
+        if(AccountStore.getInstance().getAccountByMail(mail).isPresent()){
+            //return "{ \"status\" : \"ERROR\" , \"message\" : \"Already registered e-address\" }";
+            return CheckAccountResult.ERROR_SAME;
+        }
+        if(pass.length() < 7){
+            //return "{ \"status\" : \"ERROR\" , \"message\" : \"Need 8 characters or more\" }";
+            return CheckAccountResult.ERROR_PASSWORD_LENGTH;
+        }
+        //return "{ \"status\" : \"SUCCESS\"}";
+        return CheckAccountResult.SUCCESS;
+    }
+
 
 }
