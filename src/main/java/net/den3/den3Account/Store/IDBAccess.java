592e6625f894ab8eea0a91caddf106f99f94bf7b
Kengo Wada <msf.1192@gmail.com>
2020-11-03 20:27:28 +0900

    すべてのアカウントを取得するメソッドの定義にともなってJavadocも追加

--- src/main/java/net/den3/den3Account/Store/IDBAccess.java
+++ src/main/java/net/den3/den3Account/Store/IDBAccess.java
@@ -9,7 +9,29 @@ import java.util.Optional;
 import java.util.function.Function;
 
 public interface IDBAccess {
+    /**
+     * DBに保存されたすべてのアカウントを取得する
+     * @return Optional<List<IAccount>>
+     */
+    Optional<List<IAccount>> getAccountsAll();
+
+    /**
+     * 指定したメールアドレスをもつアカウントを取得する
+     * @param mail
+     * @return Optional<IAccount></>
+     */
     Optional<IAccount> getAccountByMail(String mail);
+    /**
+     * 指定したuuidをもつアカウントを取得する
+     * @param id
+     * @return Optional<IAccount></>
+     */
     Optional<IAccount> getAccountByUUID(String id);
+
+    /**
+     * 発行したSQLに合致するアカウントを取得する
+     * @param query Connectionを引数に持ち戻り値がPreparedStatement>のラムダ式/クロージャ
+     * @return Optional<List<IAccount>>
+     */
     Optional<List<IAccount>> getAccountBySQL(Function<Connection, PreparedStatement> query);
 }
