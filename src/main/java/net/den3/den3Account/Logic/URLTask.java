4fb430e7b6e924dcffeab8ca72b27edddfca99b5
Kengo Wada <msf.1192@gmail.com>
2020-11-03 21:43:12 +0900

    URL処理直書きから登録処理用のクラスに処理を移した

--- src/main/java/net/den3/den3Account/Logic/URLTask.java
+++ src/main/java/net/den3/den3Account/Logic/URLTask.java
@@ -13,28 +13,8 @@ public class URLTask {
                 if(!req.params().containsKey("mail") ||  !req.params().containsKey("pass")){
                     return "{ status : \"Client Error\" }";
                 }
-                //mail 生passwordをリクエストから取り出す
-                String mail = req.params().get("mail");
-                String raw_pass = req.params().get("pass");
-
-                //基準に満たない/ルール違反をしているメールアドレス/パスワードか調べる
-                CheckAccountResult checkAccountResult = RegisterAccount.checkAccount(req.params().get("mail"), req.params().get("pass"));
-
-                //チェックにひっかかるアカウント情報ならばここで弾く
-                if(checkAccountResult != CheckAccountResult.SUCCESS){
-                    //ここに到達するときは登録処理に失敗している
-                    return "{ \"status\" : \"ERROR\" , \"message\" : \"" +checkAccountResult.getString() + "\" }";
-                }
-
-                //<-- ここまでで基準に満たないアカウント登録はすべて却下されている -->
-
-                //TODO ここに仮登録処理を書く
-                TemporaryAccountEntity tempAccount = TemporaryAccountEntity.create(mail, raw_pass);
-                //ここで待機させておいて, ここで発行したキーの書かれたメールからアクセスされたリンクをもとに有効化判定をする
-                String tempKey = RegisterAccount.addQueueDBRegister(tempAccount);
-                //TODO 登録されたメールアドレスにメールを送信する
-
-                return "{ \"status\" : \"SUCCESS\" , \"message\" : \"" +tempKey + "\" }";
+                //仮登録処理をぶん投げる
+                return RegisterAccount.entryFlow(req.params().get("mail"),req.params().get("pass"));
             });
         });
     }
