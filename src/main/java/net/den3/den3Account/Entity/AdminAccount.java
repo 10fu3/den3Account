b6243778e1039db6022426ae10956b0096b49e6f
Kengo Wada <msf.1192@gmail.com>
2020-11-05 18:30:56 +0900

    クローン機能を実装

--- src/main/java/net/den3/den3Account/Entity/AdminAccount.java
+++ src/main/java/net/den3/den3Account/Entity/AdminAccount.java
@@ -4,4 +4,13 @@ package net.den3.den3Account.Entity;
  * 管理者を表すクラス
  */
 
-public class AdminAccount extends AccountEntity implements IAccount {}
+public class AdminAccount extends AccountEntity implements IAccount {
+    AdminAccount(AccountEntity old){
+        super.setMailAddress(old.getMailAddress());
+        super.setIconURL(old.getIconURL());
+        super.setPasswordHash(old.getPasswordHash());
+        super.setLastLogin(old.getLastLoginTime());
+        super.setNickName(old.getNickName());
+        super.setUUID(old.getUUID());
+    }
+}
