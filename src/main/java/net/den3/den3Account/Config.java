3ca036c32205b4cc439ae2b333dee23bc2c61553
Kengo Wada <msf.1192@gmail.com>
2020-11-06 17:02:45 +0900

    Docker上での起動によってアクセスするURLを変更した

--- src/main/java/net/den3/den3Account/Config.java
+++ src/main/java/net/den3/den3Account/Config.java
@@ -2,8 +2,9 @@ package net.den3.den3Account;
 
 public class Config {
     private final static Config config = new Config();
-    private String DBAccountName = "";
-    private String DBAccountPassword = "";
+    private String DBAccountName;
+    private String DBAccountPassword;
+    private Boolean DockerMODE;
 
     public static Config get(){
         return config;
@@ -12,6 +13,7 @@ public class Config {
     public Config(){
         this.DBAccountName = System.getenv("D3A_DBACCOUNT");
         this.DBAccountPassword = System.getenv("D3A_DBPASSWORD");
+        this.DockerMODE = (System.getenv("D3A_ISDOCKER") != null) && System.getenv("D3A_ISDOCKER").equalsIgnoreCase("DOCKER");
     }
 
     public String getDBAccountName() {
@@ -20,5 +22,13 @@ public class Config {
 
     public String getDBAccountPassword() {
         return DBAccountPassword;
+    }
+
+    public String getDBURL(){
+        return this.DockerMODE ? "jdbc:mariadb://db:3306/den3_account" : "jdbc:mariadb://localhost:3306/den3_account";
+    }
+
+    public String getRedisURL(){
+        return this.DockerMODE ? "redis" : "localhost";
     }
 }
