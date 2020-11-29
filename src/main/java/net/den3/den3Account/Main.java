8bd62f2914b46fbf7ef5ffbd3eb107f54bdb0aab
Kengo Wada <msf.1192@gmail.com>
2020-11-29 16:46:05 +0900

    コマンドの受付待機とルーティング設定メソッドの呼び出しを実装

--- src/main/java/net/den3/den3Account/Main.java
+++ src/main/java/net/den3/den3Account/Main.java
@@ -1,7 +1,10 @@
 package net.den3.den3Account;
 
+import net.den3.den3Account.Command.CommandExecutor;
+import net.den3.den3Account.Router.URLTask;
 public class Main {
     public static void main(String[] args) {
-
+        new Thread(CommandExecutor.SINGLE::standbyCommand).start();
+        URLTask.setupRouting();
     }
 }
