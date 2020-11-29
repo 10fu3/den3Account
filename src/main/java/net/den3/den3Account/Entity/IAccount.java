08a554b188d8a1140d5df059c68d8a480f06aef8
Kengo Wada <msf.1192@gmail.com>
2020-11-29 14:37:06 +0900

    setterを定義

--- src/main/java/net/den3/den3Account/Entity/IAccount.java
+++ src/main/java/net/den3/den3Account/Entity/IAccount.java
@@ -5,9 +5,14 @@ import java.util.Date;
 public interface IAccount {
     String getUUID();
     String getMailAddress();
+    IAccount setMailAddress(String mail);
     String getPasswordHash();
+    IAccount setPasswordHash(String pass);
     String getNickName();
+    IAccount setNickName(String nick);
     String getIconURL();
+    IAccount setIconURL(String url);
     String getLastLoginTime();
+    IAccount setLastLogin(String time);
     String toString();
 }
