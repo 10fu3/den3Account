5004b323c6cd9622f626bcca1fef171fefd01d41
Kengo Wada <msf.1192@gmail.com>
2020-11-29 16:16:40 +0900

    JSONパースクラスを実装

---
+++ src/main/java/net/den3/den3Account/Logic/ParseJSON.java
@@ -0,0 +1,30 @@
+package net.den3.den3Account.Logic;
+
+import net.den3.den3Account.Entity.IAccount;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ParseJSON{
+    public static String parse(List<IAccount> lae){
+        return new StringBuilder()
+                .append("[ ")
+                .append(lae.stream().map(ae-> new StringBuilder()
+                .append(ae.toString())
+                .toString()).collect(Collectors.joining(",")))
+                .append("]")
+                .toString();
+
+    }
+
+    public static StringBuilder buildWord(String key,String value){
+        return new StringBuilder()
+                .append("\"")
+                .append(key)
+                .append("\"")
+                .append(" : ")
+                .append("\"")
+                .append(value)
+                .append("\"");
+    }
+}
