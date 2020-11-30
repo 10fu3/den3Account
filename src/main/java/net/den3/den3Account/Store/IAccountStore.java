7d26a23187e288cca46d59de9c5e51711e93d3f3
Kengo Wada <msf.1192@gmail.com>
2020-11-30 22:32:06 +0900

    データストアからの機能の切り出しによって生じた変更をインターフェースにも反映

---
+++ src/main/java/net/den3/den3Account/Store/IAccountStore.java
@@ -0,0 +1,65 @@
+package net.den3.den3Account.Store;
+
+import net.den3.den3Account.Entity.IAccount;
+import net.den3.den3Account.Entity.TemporaryAccountEntity;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+public interface IAccountStore {
+    /**
+     * シングルトンオブジェクトを取得する
+     * @return アカウントストア
+     */
+    static IAccountStore getInstance() {
+        return AccountStore.getInstance();
+    }
+
+    /**
+     * アカウントの情報を更新する
+     *
+     * @param account 更新するエンティティ
+     * @return true → 成功 false → 失敗
+     */
+    boolean updateAccountInSQL(IAccount account);
+    /**
+     * アカウントをDBに登録する
+     *
+     * @param tempAccount 仮アカウントエンティティ
+     * @return 登録されたアカウントエンティティ
+     */
+    Optional<IAccount> addAccountInSQL(TemporaryAccountEntity tempAccount);
+    /**
+     * アカウントをDBから削除する
+     *
+     * @param deleteAccount 削除対象のアカウントエンティティ
+     * @return true → 削除成功 false → 失敗
+     */
+    boolean deleteAccountInSQL(IAccount deleteAccount);
+    /**
+     * データベースに登録されたアカウントをすべて取得する
+     * @return アカウントエンティティのリスト
+     */
+    Optional<List<IAccount>> getAccountsAll();
+    /**
+     * データベースの中にあるアカウントをメールアドレスで検索して取得する
+     * @param mail
+     * @return アカウントエンティティ
+     */
+    Optional<IAccount> getAccountByMail(String mail);
+    /**
+     * データベースの中にあるアカウントをuuidで検索して取得する
+     * @param id
+     * @return アカウントエンティティ
+     */
+    Optional<IAccount> getAccountByUUID(String id);
+    /**
+     * 発行したSQLに合致するアカウントを取得する
+     * @param query Connectionを引数に持ち戻り値がPreparedStatement>のラムダ式/クロージャ
+     * @return
+     */
+    Optional<List<IAccount>> getAccountBySQL(Function<Connection, Optional<PreparedStatement>> query);
+}
