b53043c4a45435e768cbe5701ad41e8d84182b38
Kengo Wada <msf.1192@gmail.com>
2020-11-30 22:13:19 +0900

    adminは予約語だったのでpermissionに変更

--- initdb/init.sql
+++ initdb/init.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS den3_account;
 USE DATABASE den3_account;
 DROP TABLE IF EXISTS account_repository;
-create table account_repository(uuid VARCHAR(255) PRIMARY KEY,mail VARCHAR(255),pass VARCHAR(255),nick VARCHAR(255),icon VARCHAR(255),last_login_time VARCHAR(255),admin VARCHAR(255));
+create table account_repository(uuid VARCHAR(255) PRIMARY KEY,mail VARCHAR(255),pass VARCHAR(255),nick VARCHAR(255),icon VARCHAR(255),last_login_time VARCHAR(255),permission VARCHAR(255));
 USE TABLE account_repository;
 insert into account_repository values ('SYS_ADMIN','ADMIN_MAIL','ADMIN_PASS','ADMIN','https://i.imgur.com/R6tktJ6.jpg','2020/11/04 17:13:00','ADMIN');
