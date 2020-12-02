9edfeaeea1832e263d06b754b9cf3a5871d9fd03
Kengo Wada <msf.1192@gmail.com>
2020-12-02 11:10:03 +0900

    メールのタイトル・本文のメールエンティティを実装

---
+++ src/main/java/net/den3/den3Account/Entity/IMailEntity.java
@@ -0,0 +1,16 @@
+package net.den3.den3Account.Entity;
+
+public interface IMailEntity {
+    static IMailEntity getInstance() {
+        return new MailEntity();
+    }
+
+    IMailEntity setTo(String toAddress);
+    String getTo();
+    IMailEntity setBody(String body);
+    String getBody();
+    IMailEntity setTitle(String title);
+    String getTitle();
+    IMailEntity setName(String name);
+    String getName();
+}
---
+++ src/main/java/net/den3/den3Account/Entity/MailEntity.java
@@ -0,0 +1,52 @@
+package net.den3.den3Account.Entity;
+
+public class MailEntity implements IMailEntity{
+    private String title;
+    private String body;
+    private String toAddress;
+    private String name;
+
+    @Override
+    public IMailEntity setTo(String toAddress) {
+        this.toAddress = toAddress;
+        return this;
+    }
+
+    @Override
+    public String getTo() {
+        return this.toAddress;
+    }
+
+    @Override
+    public IMailEntity setBody(String body) {
+        this.body = body;
+        return this;
+    }
+
+    @Override
+    public String getBody() {
+        return this.body;
+    }
+
+    @Override
+    public IMailEntity setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    @Override
+    public String getTitle() {
+        return this.title;
+    }
+
+    @Override
+    public IMailEntity setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+}
