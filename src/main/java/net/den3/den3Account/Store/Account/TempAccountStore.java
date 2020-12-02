fd76a6112d26f12ffb4e6fc897f55555cb41cfdc
Kengo Wada <msf.1192@gmail.com>
2020-12-03 00:37:26 +0900

    テーブルがないときに自動的に作成する処理を実装

--- src/main/java/net/den3/den3Account/Store/Account/TempAccountStore.java
+++ src/main/java/net/den3/den3Account/Store/Account/TempAccountStore.java
@@ -19,6 +19,25 @@ public class TempAccountStore implements ITempAccountStore {
         return SINGLE;
     }
 
+    public TempAccountStore(){
+        store.controlSQL((con)->{
+            try{
+                return Optional.of(Collections.singletonList(
+                        //テーブルがなかったら作る仕組み
+                        con.prepareStatement("CREATE TABLE IF NOT EXISTS temp_account_repository ("
+                                +"active_key VARCHAR(256) PRIMARY KEY, "
+                                +"valid_date VARCHAR(256), "
+                                +"mail VARCHAR(256), "
+                                +"pass VARCHAR(256), "
+                                +"nick VARCHAR(256)"
+                                +");")));
+            }catch (SQLException ex){
+                ex.printStackTrace();
+                return Optional.empty();
+            }
+        });
+    }
+
     /**
      * データベースに登録されたアカウントの中に指定した有効化キーを持つアカウントがあるか探す
      *
@@ -27,17 +46,16 @@ public class TempAccountStore implements ITempAccountStore {
      */
     @Override
     public boolean containsAccount(String key) {
-        Optional<List<Map<String, String>>> optionalList = store.getLineBySQL((con) -> {
+        Optional<List<Map<String, String>>> optionalList = store.getLineBySQL(Collections.singletonList("active_key"),(con) -> {
             try {
                 //account_repositoryからmailの一致するものを探してくる
-                PreparedStatement pS = con.prepareStatement("SELECT * FROM temp_account_repository WHERE key = ?");
+                PreparedStatement pS = con.prepareStatement("SELECT * FROM temp_account_repository WHERE active_key = ?;");
                 pS.setString(1,key);
                 return Optional.of(pS);
             } catch (SQLException sqlex) {
-                sqlex.printStackTrace();
                 return Optional.empty();
             }
-        },"temp_account_repository");
+        });
         return optionalList.isPresent() && optionalList.get().size() == 1;
     }
 
@@ -49,22 +67,24 @@ public class TempAccountStore implements ITempAccountStore {
      */
     @Override
     public Optional<ITempAccount> getAccount(String key) {
-        Optional<List<Map<String, String>>> optionalList = store.getLineBySQL((con) -> {
+        List<String> columns = Arrays.asList("active_key","valid_date","mail","pass","nick");
+        Optional<List<Map<String, String>>> optionalList = store.getLineBySQL(columns,(con) -> {
             try {
                 //account_repositoryからmailの一致するものを探してくる
-                PreparedStatement pS = con.prepareStatement("SELECT * FROM temp_account_repository WHERE key = ?");
+                PreparedStatement pS = con.prepareStatement("SELECT * FROM temp_account_repository WHERE active_key = ?;");
+                pS.setString(1,key);
                 return Optional.of(pS);
             } catch (SQLException sqlex) {
                 sqlex.printStackTrace();
                 return Optional.empty();
             }
-        },"temp_account_repository");
+        });
         if(!optionalList.isPresent()){
             return Optional.empty();
         }
         Map<String, String> map = optionalList.get().stream().findAny().get();
         ITempAccount a = new TemporaryAccountEntity();
-        a.setKey(map.get("key"))
+        a.setKey(map.get("active_key"))
          .setRegisteredDate(map.get("valid_date"))
          .setMail(map.get("mail"))
          .setPasswordHash(map.get("pass"))
@@ -83,17 +103,17 @@ public class TempAccountStore implements ITempAccountStore {
         return store.controlSQL((con)->{
             try {
                 //INSET文の発行 uuid mail pass nick icon last_login_timeの順
-                PreparedStatement pS = con.prepareStatement("INSERT INTO temp_account_repository VALUES (?,?,?,?) ;");
+                PreparedStatement pS = con.prepareStatement("INSERT INTO temp_account_repository VALUES (?,?,?,?,?) ;");
                 //
                 pS.setString(1, tempAccount.getKey());
                 //
                 pS.setString(2, tempAccount.getRegisteredDate());
                 //
-                pS.setString(2, tempAccount.getMail());
+                pS.setString(3, tempAccount.getMail());
                 //SQL文の1個めの?にmailを代入する
-                pS.setString(3, tempAccount.getPasswordHash());
+                pS.setString(4, tempAccount.getPasswordHash());
                 //SQL文の1個めの?にmailを代入する
-                pS.setString(4, tempAccount.getNickName());
+                pS.setString(5, tempAccount.getNickName());
                 return Optional.of(Collections.singletonList(pS));
             } catch (SQLException sqlex) {
                 sqlex.printStackTrace();
@@ -109,19 +129,20 @@ public class TempAccountStore implements ITempAccountStore {
      */
     @Override
     public Optional<List<ITempAccount>> getAccountsAll() {
-        Optional<List<Map<String, String>>> optionalList = store.getLineBySQL((con) -> {
+        List<String> columns = Arrays.asList("active_key","valid_date","mail","pass","nick");
+        Optional<List<Map<String, String>>> optionalList = store.getLineBySQL(columns,(con) -> {
             try {
                 //account_repositoryからmailの一致するものを探してくる
-                PreparedStatement pS = con.prepareStatement("SELECT * FROM temp_account_repository");
+                PreparedStatement pS = con.prepareStatement("SELECT * FROM temp_account_repository;");
                 return Optional.of(pS);
             } catch (SQLException sqlex) {
                 sqlex.printStackTrace();
                 return Optional.empty();
             }
-        },"temp_account_repository");
+        });
         return optionalList.map(list-> list.stream().map(map->{
             ITempAccount a = new TemporaryAccountEntity();
-            a.setKey(map.get("key"))
+            a.setKey(map.get("active_key"))
              .setRegisteredDate(map.get("valid_date"))
              .setPasswordHash(map.get("pass"))
              .setNickName(map.get("nick"));
@@ -137,6 +158,15 @@ public class TempAccountStore implements ITempAccountStore {
      */
     @Override
     public boolean removeAccountInTemporaryDB(String key) {
-        return false;
+        return store.controlSQL(con->{
+            try{
+                PreparedStatement ps = con.prepareStatement("DELETE FROM temp_account_repository WHERE active_key = ?");
+                ps.setString(1,key);
+                return Optional.of(Collections.singletonList(ps));
+            }catch (SQLException ex){
+                ex.printStackTrace();
+                return Optional.empty();
+            }
+        });
     }
 }
