7f13d61a1068b8cdb37c3caee8843b30166e21b1
Kengo Wada <msf.1192@gmail.com>
2020-12-02 16:08:21 +0900

    テーブルの列名として使えるように変更

--- src/main/java/net/den3/den3Account/Entity/ServicePermission.java
+++ src/main/java/net/den3/den3Account/Entity/ServicePermission.java
@@ -1,11 +1,16 @@
 package net.den3.den3Account.Entity;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public enum  ServicePermission {
     READ_UUID("read_uuid"),
     READ_MAIL("read_mail"),
     READ_PROFILE("read_profile"),
     READ_LAST_LOGIN_TIME("read_last_login_time");
 
+    public static final List<String> names = Collections.unmodifiableList(Arrays.asList("read_uuid","read_mail","read_profile","read_last_login_time"));
     private final String name;
     ServicePermission(String perm){
         this.name = perm;
