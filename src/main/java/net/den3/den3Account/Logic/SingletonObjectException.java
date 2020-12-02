6eccf63cb62e39f1c0112d772e872e02247361f3
Kengo Wada <msf.1192@gmail.com>
2020-12-02 14:14:50 +0900

    使っていたなかった例外を削除

--- src/main/java/net/den3/den3Account/Logic/SingletonObjectException.java
+++
@@ -1,7 +0,0 @@
-package net.den3.den3Account.Logic;
-
-public class SingletonObjectException extends RuntimeException{
-    public SingletonObjectException(){
-        super("シングルトンオブジェクトを含むクラスのインスタンスを生成しないでください");
-    }
-}
