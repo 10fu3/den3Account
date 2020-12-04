83efc30fa9be770f474b6465c0f3890efe42aea1
Kengo Wada <msf.1192@gmail.com>
2020-12-04 23:29:32 +0900

    Enumに名前を持たせた

--- src/main/java/net/den3/den3Account/Entity/Permission.java
+++ src/main/java/net/den3/den3Account/Entity/Permission.java
@@ -3,6 +3,14 @@ package net.den3.den3Account.Entity;
 /**
  * アカウントエンティティにつくロール
  */
-public enum Permission{
-    ADMIN,NORMAL
+public enum Permission implements io.javalin.core.security.Role{
+    ADMIN("admin"),NORMAL("normal");
+    private final String name;
+    Permission(final String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
