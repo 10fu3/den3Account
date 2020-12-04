423c584b9f8d887ebfe31122aeda6c6a48bfd8e4
10fu3 <msf.1192@gmail.com>
2020-12-05 03:28:46 +0900

    エラーメッセージをJSONで返すように実装

--- src/main/java/net/den3/den3Account/Router/URLLogin.java
+++ src/main/java/net/den3/den3Account/Router/URLLogin.java
@@ -1,8 +1,10 @@
 package net.den3.den3Account.Router;
 
+import net.den3.den3Account.Entity.LoginResult;
 import net.den3.den3Account.Logic.LoginAccount;
 import net.den3.den3Account.Logic.ParseJSON;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -13,12 +15,24 @@ class URLLogin {
 
     static void mainFlow(io.javalin.http.Context ctx){
         Optional<Map<String, String>> wrapJson = ParseJSON.convertToMap(ctx.body());
-        if(!wrapJson.isPresent()){
-            ctx.json("{\"STATUS\" : \"JSON format error\"}");
-        }else if(!containsNeedKey(wrapJson.get())){
-            ctx.json("{\"STATUS\" : \"JSON format error\"}");
+        Map<String,Object> mes = new HashMap<>();
+        mes.put("STATUS","ERROR");
+        if(!wrapJson.isPresent() || !containsNeedKey(wrapJson.get())){
+            mes.put("MESSAGE","JSON format error");
         }else{
-            LoginAccount.mainFlow(wrapJson.get());
+            LoginResult result = LoginAccount.mainFlow(wrapJson.get());
+            if(result != LoginResult.SUCCESS){
+                mes.put("MESSAGE",result.getMessage());
+            }else{
+
+            }
+        }
+        Optional<String> messageJSON = ParseJSON.convertToJSON(mes);
+        if(messageJSON.isPresent()){
+            ctx.status(401).json(messageJSON.get());
+        }else {
+            //もし500を返すのであれば,上が呼び出しているメソッドの実装を間違えている
+            ctx.status(500);
         }
     }
 }
