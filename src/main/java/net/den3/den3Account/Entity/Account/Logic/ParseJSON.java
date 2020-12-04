fa823416fcf200be2ba03afacf23004b49f2584d
Kengo Wada <msf.1192@gmail.com>
2020-12-04 23:42:55 +0900

    JSON変換機能を実装

---
+++ src/main/java/net/den3/den3Account/Entity/Account/Logic/ParseJSON.java
@@ -0,0 +1,61 @@
+package net.den3.den3Account.Entity.Account.Logic;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.den3.den3Account.Entity.Account.IAccount;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class ParseJSON{
+    public static String parse(List<IAccount> lae){
+        return new StringBuilder()
+                .append("[ ")
+                .append(lae.stream().map(ae-> new StringBuilder()
+                .append(ae.toString())
+                .toString()).collect(Collectors.joining(",")))
+                .append("]")
+                .toString();
+
+    }
+
+    public static StringBuilder buildWord(String key,String value){
+        return new StringBuilder()
+                .append("\"")
+                .append(key)
+                .append("\"")
+                .append(" : ")
+                .append("\"")
+                .append(value)
+                .append("\"");
+    }
+
+    public static Optional<Map<String,Object>> convertToMap(String json){
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map = new HashMap<>();
+        try {
+            map = mapper.readValue(json,Map.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+        if(map.size() == 0){
+            return Optional.empty();
+        }else{
+            return Optional.of(map);
+        }
+    }
+
+    public static Optional<String> convertToJSON(Map<String,Object> map){
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return Optional.of(mapper.writeValueAsString(map));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+}
