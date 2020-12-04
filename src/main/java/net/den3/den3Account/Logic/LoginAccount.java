3ac83b491d1caa81570d7ab23a6cb34af02bd09a
10fu3 <msf.1192@gmail.com>
2020-12-05 02:51:20 +0900

    パスワードの比較まで実装

--- src/main/java/net/den3/den3Account/Logic/LoginAccount.java
+++ src/main/java/net/den3/den3Account/Logic/LoginAccount.java
@@ -1,13 +1,47 @@
 package net.den3.den3Account.Logic;
 
+import net.den3.den3Account.Entity.Account.IAccount;
+import net.den3.den3Account.Entity.LoginResult;
+import net.den3.den3Account.Security.PasswordHashGenerator;
+import net.den3.den3Account.Store.Account.IAccountStore;
+
 import java.util.Map;
 import java.util.Optional;
 
 public class LoginAccount {
-    public Optional<Boolean> containsStore(Map<String,String> json){
+    public LoginResult containsStore(Map<String,String> json){
         String mail = json.get("mail");
-        String pass = json.get("pass");
+        Optional<IAccount> account = IAccountStore.getInstance().getAccountByMail(mail);
+        LoginResult result;
+        if(account.isPresent()){
+            result = LoginResult.SUCCESS;
+            result.account = account.get();
+        }else{
+            result = LoginResult.ERROR_NOT_EXIST;
+        }
+        return result;
+    }
 
-        return Optional.of(false);
+    /**
+     * パスワードが一致するか確認する
+     * @param json 送られてきたJSON メールとパスワードが含まれている前提
+     * @param before containsStoreで検査した結果
+     * @return LoginResult
+     */
+    public LoginResult authenticateAccount(Map<String,String> json, LoginResult before){
+        LoginResult result = before;
+        String pass = json.get("pass");
+        if(result != LoginResult.SUCCESS || pass == null){
+            return before;
+        }
+        IAccount resultAccount = result.account;
+        String generatePassword = PasswordHashGenerator.getSafetyPassword(pass,resultAccount.getUUID());
+        if(resultAccount.getPasswordHash().equalsIgnoreCase(generatePassword)){
+            return result;
+        }else{
+            result = LoginResult.ERROR_WRONGPASS;
+            result.account = null;
+            return result;
+        }
     }
 }
