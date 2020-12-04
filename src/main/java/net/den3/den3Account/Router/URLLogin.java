aa642a33d9c3093288f8f7b921cde82062664bf5
Kengo Wada <msf.1192@gmail.com>
2020-12-04 23:30:32 +0900

    ログイン時のルーティングを定義

---
+++ src/main/java/net/den3/den3Account/Router/URLLogin.java
@@ -0,0 +1,20 @@
+package net.den3.den3Account.Router;
+
+import net.den3.den3Account.Entity.Account.Logic.ParseJSON;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class URLLogin {
+    public static Boolean containsNeedKey(Map<String,Object> json){
+        return json.containsKey("mail") ||  json.containsKey("pass");
+    }
+
+    public static void mainFlow(io.javalin.http.Context ctx){
+        Optional<Map<String, Object>> wrapJson = ParseJSON.convertToMap(ctx.body());
+        if(!wrapJson.isPresent()){
+            ctx.json("{\"STATUS\" : \"JSON format error\"}");
+            return;
+        }
+    }
+}
