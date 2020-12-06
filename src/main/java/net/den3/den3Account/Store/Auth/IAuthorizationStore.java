ecb936c83c85d204e084db2cde1eb36639e3c9e4
10fu3 <msf.1192@gmail.com>
2020-12-07 01:52:41 +0900

    Javadocを訂正

--- src/main/java/net/den3/den3Account/Store/Auth/IAuthorizationStore.java
+++ src/main/java/net/den3/den3Account/Store/Auth/IAuthorizationStore.java
@@ -46,7 +46,7 @@ public interface IAuthorizationStore {
 
     /**
      * アカウントをアプリの個人情報使用認可ストアから削除する
-     * @param account 追加するアカウント
+     * @param account 削除するアカウント
      * @param service 削除先のサービス
      * @return true->成功 false->失敗
      */
