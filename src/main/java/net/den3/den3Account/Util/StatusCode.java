1ea6601e52fc8232b226de8b7003b9825b37ed3a
10fu3 <msf.1192@gmail.com>
2020-12-12 00:33:28 +0900

    HTTPのステータスコードをenumで表現

---
+++ src/main/java/net/den3/den3Account/Util/StatusCode.java
@@ -0,0 +1,57 @@
+package net.den3.den3Account.Util;
+
+public enum StatusCode {
+    Continue(100),
+    SwitchingProtocols (101),
+
+    OK                   (200),
+    Created              (201),
+    Accepted             (202),
+    NonAuthoritativeInfo (203),
+    NoContent            (204),
+    ResetContent         (205),
+    PartialContent       (206),
+
+    MultipleChoices   (300),
+    MovedPermanently  (301),
+    Found             (302),
+    SeeOther          (303),
+    NotModified       (304),
+    UseProxy          (305),
+    TemporaryRedirect (307),
+
+    BadRequest                   (400),
+    Unauthorized                 (401),
+    PaymentRequired              (402),
+    Forbidden                    (403),
+    NotFound                     (404),
+    MethodNotAllowed             (405),
+    NotAcceptable                (406),
+    ProxyAuthRequired            (407),
+    RequestTimeout               (408),
+    Conflict                     (409),
+    Gone                         (410),
+    LengthRequired               (411),
+    PreconditionFailed           (412),
+    RequestEntityTooLarge        (413),
+    RequestURITooLong            (414),
+    UnsupportedMediaType         (415),
+    RequestedRangeNotSatisfiable (416),
+    ExpectationFailed            (417),
+
+    InternalServerError     (500),
+    NotImplemented          (501),
+    BadGateway              (502),
+    ServiceUnavailable      (503),
+    GatewayTimeout          (504),
+    HTTPVersionNotSupported (505);
+    private int statusCode;
+
+    StatusCode(int statusCode){
+        this.statusCode  = statusCode;
+    }
+
+    public int code() {
+        return statusCode;
+    }
+}
