02b71f5ca876d96b78f821a067fc3d3abcc02dc3
10fu3 <msf.1192@gmail.com>
2020-12-12 02:37:40 +0900

    JWTの署名を検証する処理の実装

---
+++ src/main/java/net/den3/den3Account/Security/JWTVerify.java
@@ -0,0 +1,23 @@
+package net.den3.den3Account.Security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.util.Optional;
+
+public class JWTVerify {
+    public static Optional<DecodedJWT> check(String token, String secret, String serverID){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(serverID)
+                    .build(); //Reusable verifier instance
+            return Optional.ofNullable(verifier.verify(token));
+        } catch (JWTVerificationException exception){
+            return Optional.empty();
+        }
+    }
+}
