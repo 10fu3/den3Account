b009475796fb97101291f3eb55f2a00f9a1a8361
10fu3 <msf.1192@gmail.com>
2020-12-13 02:38:34 +0900

    権限を文字列から探し出す実装を追加

--- src/main/java/net/den3/den3Account/Entity/Permission.java
+++ src/main/java/net/den3/den3Account/Entity/Permission.java
@@ -4,12 +4,22 @@ package net.den3.den3Account.Entity;
  * アカウントエンティティにつくロール
  */
 public enum Permission{
-    ADMIN("admin"),NORMAL("normal"),DEVELOPER("developer"),SUSPEND("suspend"),ERROR("error");
+    ADMIN("ADMIN"),NORMAL("NORMAL"),DEVELOPER("DEVELOPER"),SUSPEND("SUSPEND"),ERROR("ERROR");
     private final String name;
+    private static Permission[] ps = Permission.values();
     Permission(final String name){
         this.name = name;
     }
     public String getName() {
         return name;
+    }
+    public static Permission getRole(String tag){
+        Permission p = Permission.ERROR;
+        for (int i = 0; i < ps.length; i++) {
+            if(ps[i].getName().equalsIgnoreCase(tag)){
+                p = ps[i];
+            }
+        }
+        return p;
     }
 }
