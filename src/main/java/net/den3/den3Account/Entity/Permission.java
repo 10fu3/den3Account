059d00e861b96b8df4838fbe3650e1f41886a810
10fu3 <msf.1192@gmail.com>
2020-12-12 23:12:14 +0900

    権限の表す内容を追加

--- src/main/java/net/den3/den3Account/Entity/Permission.java
+++ src/main/java/net/den3/den3Account/Entity/Permission.java
@@ -3,13 +3,12 @@ package net.den3.den3Account.Entity;
 /**
  * アカウントエンティティにつくロール
  */
-public enum Permission implements io.javalin.core.security.Role{
-    ADMIN("admin"),NORMAL("normal");
+public enum Permission{
+    ADMIN("admin"),NORMAL("normal"),DEVELOPER("developer"),SUSPEND("suspend"),ERROR("error");
     private final String name;
     Permission(final String name){
         this.name = name;
     }
-
     public String getName() {
         return name;
     }
