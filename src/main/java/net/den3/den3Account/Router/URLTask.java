de4ea30272840823b73eb53f65541265aa43cd07
10fu3 <msf.1192@gmail.com>
2020-12-13 01:55:03 +0900

    冗長な書き方を改めた

--- src/main/java/net/den3/den3Account/Router/URLTask.java
+++ src/main/java/net/den3/den3Account/Router/URLTask.java
@@ -2,6 +2,8 @@ package net.den3.den3Account.Router;
 
 import net.den3.den3Account.Router.OAuth2.URLAuthorize;
 
+import static io.javalin.apibuilder.ApiBuilder.*;
+
 public class URLTask {
 
     public static io.javalin.Javalin webApp;
@@ -11,17 +13,17 @@ public class URLTask {
         webApp = io.javalin.Javalin.create().start(8080);
 
         webApp.routes(()->{
-            io.javalin.apibuilder.ApiBuilder.path("/api/v1",()->{
-                io.javalin.apibuilder.ApiBuilder.path("/account",()->{
-                    io.javalin.apibuilder.ApiBuilder.post("/entry", URLEntryAccount::mainFlow);
-                    io.javalin.apibuilder.ApiBuilder.post("/login",URLLogin::mainFlow);
+            path("/api/v1",()->{
+                path("/account",()->{
+                    post("/entry", URLEntryAccount::mainFlow);
+                    post("/login",URLLogin::mainFlow);
                 });
             });
         });
 
         webApp.routes(()->{
-            io.javalin.apibuilder.ApiBuilder.path("/oauth2/v1",()->{
-                io.javalin.apibuilder.ApiBuilder.get("/authorize", URLAuthorize::mainFlow);
+            path("/oauth2/v1",()->{
+                get("/authorize", URLAuthorize::mainFlow);
 
             });
         });
