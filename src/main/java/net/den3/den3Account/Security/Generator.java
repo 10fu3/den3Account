4445caabfcc4c491406e27b812e4dccc938b7dd1
10fu3 <msf.1192@gmail.com>
2020-12-13 01:28:19 +0900

    必要ではなくなったので削除

--- src/main/java/net/den3/den3Account/Security/Generator.java
+++
@@ -1,8 +0,0 @@
-package net.den3.den3Account.Security;
-
-import com.auth0.jwt.algorithms.Algorithm;
-
-@FunctionalInterface
-public interface Generator <T> {
-    String generate(T obj, Algorithm algorithm);
-}
