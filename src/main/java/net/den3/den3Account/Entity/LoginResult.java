39d9513f89b8aa801ab0fdb4a7cf2490b45ea4b8
Kengo Wada <msf.1192@gmail.com>
2020-12-30 20:54:24 +0900

    無効なパラメーターを表現する実装を追加

--- src/main/java/net/den3/den3Account/Entity/LoginResult.java
+++ src/main/java/net/den3/den3Account/Entity/LoginResult.java
@@ -4,6 +4,7 @@ import net.den3.den3Account.Entity.Account.IAccount;
 
 public enum LoginResult {
     SUCCESS("SUCCESS"),
+    ERROR_PARAMS("Invalid parameter"),
     ERROR_WRONGPASS("Wrong password"),
     ERROR_NOT_EXIST("Invalid name"),
     ERROR_FROZEN_ACCOUNT("Frozen account"),
